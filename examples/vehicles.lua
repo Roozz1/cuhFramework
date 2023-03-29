3350c86c83338fea179ed6850450e4def8a94dcc
Cuh4 <roozzguy@gmail.com>
2023-03-29 17:45:53 +0100

    new example showcasing new vehicle stuff

---
+++ examples/vehicles.lua
@@ -0,0 +1,36 @@
+------------------------------------------------------------------------
+    --cuhFramework || An addon creation framework to make SW addon development easier. 
+	-- 		Created by cuh4#7366
+	--		cuhHub - Stormworks Server Hub: https://discord.gg/zTQxaZjwDr
+	--		This framework is open-source: https://github.com/Roozz1/cuhFramework
+------------------------------------------------------------------------
+--------- Example:
+-- Connect a function to the onVehicleSpawn callback
+cuhFramework.callbacks.onVehicleSpawn:connect(function(vehicle_id)
+    -- Get vehicle
+    local vehicle = cuhFramework.vehicles.getVehicleByVehicleId(vehicle_id)
+
+    -- Send a message in chat that contains some of the vehicle data
+    cuhFramework.chat.send_message("MyAddon", {
+        "A vehicle was spawned!",
+        "\nOwner: ", vehicle.properties.owner.properties.name,
+        "\nVehicle ID: ", vehicle.properties.vehicle_id,
+        "\nLoaded: ", tostring(vehicle.properties.loaded)
+    })
+end)
+
+-- Connect a function to the onVehicleLoad callback
+cuhFramework.callbacks.onVehicleLoad:connect(function(vehicle_id)
+    -- Get vehicle
+    local vehicle = cuhFramework.vehicles.getVehicleByVehicleId(vehicle_id)
+
+    -- Send a message in chat that contains some new vehicle data
+    if vehicle.properties.loaded then -- Check if the vehicle is loaded (just in-case)
+        cuhFramework.chat.send_message("MyAddon", {
+            "A vehicle was loaded!",
+            "\nOwner: ", vehicle.properties.owner.properties.name,
+            "\nVoxel Count: ", vehicle.properties.loaded_vehicle_data.voxels,
+            "\nLoaded: ", tostring(vehicle.properties.loaded)
+        })
+    end
+end)
